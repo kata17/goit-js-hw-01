0abc7cad5ca1764a4821af440cccfb18af4845a9
kata17 <150836558+kata17@users.noreply.github.com>
2024-11-04 14:42:29 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,15 @@
 function getElementWidth(content, padding, border) {
     const contentWidth = parseInt(content);
-    const paddingWidth = parseInt(padding);
-    const borderWidth = parseInt(border);
+    const paddingWidth = parseFloat(padding) * 2; 
+    const borderWidth = parseFloat(border) * 2;
   
-    return contentWidth + paddingWidth + borderWidth;
+    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+
+    return totalWidth;
   }
   
   console.log(getElementWidth("50px", "8px", "4px")); // 74
   console.log(getElementWidth("60px", "12px", "8.5px")); // 101
   console.log(getElementWidth("200px", "0px", "0px")); // 200
+  
   
